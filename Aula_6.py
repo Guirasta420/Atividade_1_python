e5055ee8d447249e83ebddbea40c39ea8268ce91
Gui <147107943+Guirasta420@users.noreply.github.com>
2024-08-28 21:54:08 -0300

    Add files via upload

---
+++ Aula_6.py
@@ -0,0 +1,92 @@
+#concatenação com ( , ) 
+
+# nome = 'julio'
+# sobrenome = 'fernadno'
+# print('Olá' nome , sobrenome)
+
+#concatenização com F-string
+
+# idade = int(input('Digite um numero'))
+# idade2 = int(input('digite um numero'))
+# cal = idade/idade2
+# print(f'A idade será{idade}, {idade2}')
+
+#Concatenização de :.3f
+
+# idade = int(input('Digite um numero'))
+# idade2 = int(input('digite um numero'))
+# cal = idade/idade2
+# print(f'A idade será{idade}, {idade2}, {cal:.3f}')
+
+# #concatenizção \n {cal:.4}
+# orint(f'''
+#       ''')
+
+#format
+
+# numero_1 = float(input('digite'))
+# numero_2 = int(input('Digite'))
+# print('numero {}'.format (numero_1+numero_2))
+
+# print('idade %s %s' %(numero_1,numero_2))
+      
+
+
+# if 10>2:
+#     print('é maoir')
+# else:
+#     print('é menor')
+
+multiplicar = 4 * 6 / 2
+print(multiplicar)
+
+soma = 5 + 7
+print(soma)
+
+elevado = 3 ** 4
+print(elevado)
+
+soma = 8 + 12 + 15
+media = soma / 3 
+print(media) 
+
+
+subtracao = (2 - 10) * 5 
+print(subtracao) 
+      
+divisao = 27 / 3 + 5 
+print(divisao)
+
+modulo = 17 % 4
+print(modulo)
+
+multiplicacao = 9 * 3 
+elevado = multiplicar ** 2
+print(elevado)
+
+ 
+raiz = 81 ** 0.5
+print(raiz)
+
+adicionar = 20 + 3 * 4 
+print(adicionar)
+
+multiplique = 15 * 2 - 7 
+print(multiplique)
+
+eleve = 5 ** 3 
+print(eleve)
+
+soma = 17 + 21 + 25
+media = soma / 3 
+print(media)
+
+multi = 11 * 2 +7
+print(multi)
+
+subtraia = 15 - 3 * 8 
+resultado = subtraia / 2
+print(resultado)
+
+elevacao = 2 ** 10
+print(elevacao)
