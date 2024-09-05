30ff81a6dba4f00cad4f94285b1357e3b742d893
Gui <147107943+Guirasta420@users.noreply.github.com>
2024-09-04 21:58:35 -0300

    Add files via upload

---
+++ Aula09.py
@@ -0,0 +1,55 @@
+
+# for i in range(1,11):
+#     print('Teste')
+
+# lista = [1,2,3,4,5,6,7,8,9,10,11,12]
+# for n in lista:
+#     print(n)
+
+# for a in range(11):
+#     print(a)
+
+# for b in list(range(1,11)):
+#     print(b)
+
+# for c in range(1,11,2):
+#     print(c)
+
+
+# senha_sy = 123
+# for n in range(1,4):
+#     minha_senha = int(input('Digite a senha:'))
+#     if minha_senha == senha_sy:
+#        print('Dentro')
+#        break
+# else:
+#        print('tente novamente')
+
+
+# import random
+
+# lista = [3,2,1]
+# numero_sorteado = random.randint(1,5)
+# for chances in lista:
+#      meu_chute = int(input('Adivinhe o numero'))
+#      if meu_chute == numero_sorteado:
+#        print('Acertou', numero_sorteado)
+#        break
+# else:
+#      print('Errou', numero_sorteado)
+#      if chances == 3:
+#           print('Suas chances acabaram')
+
+
+# lista_nome = []
+# for l in range(1,5):
+#     nome = input('Digite seu nome:')
+#     lista_nome.append(nome)
+# print(lista_nome)
+
+lista = [1,2,3]
+soma = 0
+for n in lista:
+    soma += n
+    print(soma)
+
