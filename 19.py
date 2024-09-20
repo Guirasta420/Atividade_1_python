b522fe6e78d917503a7d856e711eaf3630c5793c
Gui <147107943+Guirasta420@users.noreply.github.com>
2024-09-19 21:47:33 -0300

    Add files via upload

---
+++ 19.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+conexao = sqlite3.connect('cadastros_casa_de_apostas.db')
+cursor = conexao.cursor()
+
+#CRIAR UMA TABELA 
+
+cursor.execute('''
+       CREATE TABLE IF NOT EXISTS pessoas(
+          id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          nome TEXT NOT NULL,
+          idade INTERGER NOT NULL,
+          cidade TEXT NOT NULL
+    )
+    ''')
+
+#inserir dados na tabela
+
+nome = 'Paula'
+idade = 30
+cidade = 'São Paulo'
+
+cursor.execute('''
+        INSERT INTO pessoas (nome, idade, cidade)
+        VALUES (?, ?, ?)
+''', (nome, idade, cidade))
+
+
+# Confirmação a transação
+
+conexao.commit()
+
+# Consultar dados da tabela
+
+cursor.execute('SELECT * FROM pessoas')
+pessoas = cursor.fetchall()
+
+# Mostrar os dados consultados
+for pessoa in pessoas:
+    print(f''' ID: {pessoa[0]}, Nome: {pessoa[1]}, Idade: {pessoa[2]}, Cidade: {pessoa[3]}''')
+
+conexao.closed()
